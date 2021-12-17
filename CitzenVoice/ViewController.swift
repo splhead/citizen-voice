e5352980bd0b57cb45bd190dcb1d28ce560044e5
Silas P Ladislau <splhead@gmail.com>
2021-12-17 09:06:25 -0400

    Initial Commit

---
+++ CitzenVoice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CitzenVoice
+//
+//  Created by Splhead on 17/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
